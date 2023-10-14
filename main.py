27f57e7b14dfc2c3aede1eba5bf023f599aeeced
L1gHtFox <44618739+L1gHtFox@users.noreply.github.com>
2023-10-15 01:19:04 +0300

    fix: update main

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from ultralytics.utils.plotting import Annotator
 
 def main(model: YOLO, model_numbers: YOLO, image: np.array, image_path: str) -> str:
     results = model.predict(image)
+    file_name = ""
     for r in results:
         
         annotator = Annotator(image)
@@ -36,18 +37,8 @@ def main(model: YOLO, model_numbers: YOLO, image: np.array, image_path: str) -> 
                 for num_res in results_numbers:
                     # number = cv2.rectangle(number, (int(num_res[1]), int(num_res[3])), (int(num_res[0]), int(num_res[2])), (128, 255, 128), 2)
                     number_name = number_name + str(int(num_res[-1]))
-
-                # width = (b[2] - b[0]) / 16 
-
-                # number_first_half = image[int(b[1]):int(b[3]), int(b[0]):int(((b[0] + b[2]) / 2) + width)]
-                # number_second_half = image[int(b[1]):int(b[3]), int(((b[0] + b[2]) / 2) - width):int(b[2])]
-                # first = infer("D:/Projects/Python_Projects/Hackathons/AI-2/SVHNClassifier-PyTorch-master/model-54000.pth", number_first_half)
-                # second = infer("D:/Projects/Python_Projects/Hackathons/AI-2/SVHNClassifier-PyTorch-master/model-54000.pth", number_second_half)
-                # cv2.imwrite(f'./boxes/{first}_left.jpg', number_first_half)
-                # cv2.imwrite(f'./boxes/{second}_right.jpg', number_second_half)
-                # first = non_ten_list(first)
-                # second = non_ten_list(second)
-                # number_name = str(first + second)
+                if len(number_name) > 6:
+                    file_name = number_name
                 try:
                     cv2.imwrite(f'./boxes/{number_name}.jpg', number)  
                 except:
@@ -57,9 +48,11 @@ def main(model: YOLO, model_numbers: YOLO, image: np.array, image_path: str) -> 
             
         image = annotator.result() 
         try: 
-            cv2.imwrite(f'./results/{number_name}.jpg', image)    
+            cv2.imwrite(f'./results/{file_name}.jpg', image)    
         except:
-            cv2.imwrite('./results/no_number.jpb', image)
+            cv2.imwrite('./results/no_number.jpg', image)
+
+    return file_name
         
         
         
