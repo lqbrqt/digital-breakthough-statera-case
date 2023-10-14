571c63956f499136554ed223e6e8f52f3894e134
loinos <Loinose88@gmail.com>
2023-10-15 02:56:44 +0300

    add local request

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 from PIL import Image
+import requests
 
 
 image_file = st.file_uploader("Upload An Image",type=['png','jpeg','jpg'])
@@ -14,11 +15,12 @@ if image_file is not None:
     st.success("Saved File")
 
     # отправить запрос 
+    res = requests.post(" http://127.0.0.1:8000",files = os.path.join(os.path.dirname(os.path.abspath(__file__)),"tempDir",image_file.name)) 
     # дождаться ответа
     
     img = Image.open(image_file) # полученный файл
     st.image(img)
 
-    st.write('плейсхолдер для числа')
+    st.write(str(res))
     st.write('плейсхолдер для json')
     
