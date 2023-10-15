425eacfe106ff18bb7b555c4343dbcd473376caf
loinos <Loinose88@gmail.com>
2023-10-15 05:08:37 +0300

    test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import os
 from PIL import Image
 import requests
+import json
 
 
 image_file = st.file_uploader("Upload An Image",type=['png','jpeg','jpg'])
@@ -18,17 +19,18 @@ if image_file is not None:
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"tempDir",image_file.name),'rb') as filedata: 
       res = requests.post(" http://127.0.0.1:8000/img/",files = {'file':filedata}) 
     # дождаться ответа
-    st.write(type(res.content["image"]))
+    res = json.loads(res)
+    st.write(type(res["image"]))
 
-    res_img = requests.get('http://127.0.0.1:8000/get_res_img/'+res.content["image"]+'.jpg')
+    res_img = requests.get('http://127.0.0.1:8000/get_res_img/'+res["image"]+'.jpg')
     out = open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"tempDir","img.jpg"), "wb")
-    out.write(res_img.content["image"])
+    out.write(res_img["image"])
     out.close()
     
 
     img = Image.open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"tempDir","img.jpg")) # полученный файл
     st.image(img)
 
-    st.write(str(res.content))
+    st.write(str(res))
     st.write('плейсхолдер для json')
     
