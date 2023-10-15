b7aa89601c71b606c32b4b19b2515d909e67337a
loinos <Loinose88@gmail.com>
2023-10-15 05:12:32 +0300

    test

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ if image_file is not None:
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"tempDir",image_file.name),'rb') as filedata: 
       res = requests.post(" http://127.0.0.1:8000/img/",files = {'file':filedata}).text 
     # дождаться ответа
-    res = json.loads(res)
+    a = json.loads(res)
     st.write(type(res["image"]))
 
-    res_img = requests.get('http://127.0.0.1:8000/get_res_img/'+res["image"]+'.jpg')
+    res_img = requests.get('http://127.0.0.1:8000/get_res_img/'+a["image"]+'.jpg')
     out = open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"tempDir","img.jpg"), "wb")
     out.write(res_img["image"])
     out.close()
